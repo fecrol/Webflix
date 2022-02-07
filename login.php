d873fd347b87c1559b73b3ad6a73b1ab2491d1a1
Maciej <maciejfec1996@gmail.com>
2022-02-07 22:50:25 +0000

    added correct responsive column classes to divs

--- login.php
+++ login.php
@@ -16,15 +16,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    
     <link rel="stylesheet" href="./styles.css">
     <title>Login</title>
 </head>
     <body>
-        <div class="container">
-            <div class="login-container">
-                <div class="login-form">
+        <div class="col-lg-12 col-md-12">
+            <div class="login-content-container col-lg-6 center">
+                <div class="login-content col-lg-8">
+                <h1>Unlimited films, TV programmes and more.</h1>
+                <p>Whatch anywhere. Cancel at any time.</p>
+                <p>Ready to watch?</p>
+            </div>
+            </div>
+            <div class="login-container col-lg-6 col-md-12 center">
+                <div class="login-form col-lg-8 col-md-8">
                     <h1>Login</h1>
                     <form action="">
                         <p>Email</p>
@@ -34,13 +40,9 @@
                         <input type="submit" name="" value="Login">
                         <a href="#">Forgotten Password?</a>
                         <br>
-                        <p>New to Webflix? <a href="#">Sign up now</a></p>
+                        <p class="sign-up-link">New to Webflix? <a href="#">Sign up now</a></p>
                     </form>
                 </div>
-            </div>
-            <div class="login-content">
-                <h1>Unlimited films, TV programmes and more.</h1>
-                <h2>Whatch anywhere. Cancel at any time.</h2>
             </div>
         </div>
     </body>
