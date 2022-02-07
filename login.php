7ed0a2f6125f9582182c5dfe37053892b3ada2de
Maciej <maciejfec1996@gmail.com>
2022-02-07 20:31:49 +0000

    added login form and some text content

--- login.php
+++ login.php
@@ -10,6 +10,40 @@
     <title>Login</title>
 </head>
 <body>
-    
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./styles.css">
+    <title>Login</title>
+</head>
+    <body>
+        <div class="container">
+            <div class="login-container">
+                <div class="login-form">
+                    <h1>Login</h1>
+                    <form action="">
+                        <p>Email</p>
+                        <input type="text" name="email" placeholder="Enter Email">
+                        <p>Password</p>
+                        <input type="password" name="password" placeholder="Enter Password">
+                        <input type="submit" name="" value="Login">
+                        <a href="#">Forgotten Password?</a>
+                        <br>
+                        <p>New to Webflix? <a href="#">Sign up now</a></p>
+                    </form>
+                </div>
+            </div>
+            <div class="login-content">
+                <h1>Unlimited films, TV programmes and more.</h1>
+                <h2>Whatch anywhere. Cancel at any time.</h2>
+            </div>
+        </div>
+    </body>
+</html>
 </body>
 </html>
