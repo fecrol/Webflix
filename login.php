c04147d73634996237af695a0556c7184d5d9d96
Maciej <maciejfec1996@gmail.com>
2022-02-08 22:33:14 +0000

    added html image tag to add logo. added correct classes to image tag

--- login.php
+++ login.php
@@ -1,33 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="./styles.css">
-    <title>Login</title>
-</head>
-<body>
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link rel="stylesheet" href="./styles.css">
-    <title>Login</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="./styles.css">
+        <title>Login</title>
+    </head>
     <body>
         <div class="col-lg-12 col-md-12">
             <div class="login-content-container col-lg-6 center">
                 <div class="login-content col-lg-8">
-                <h1>Unlimited films, TV programmes and more.</h1>
-                <p>Whatch anywhere. Cancel at any time.</p>
-                <p>Ready to watch?</p>
-            </div>
+                    <img src="./img/logo.png" alt="Webflix logo" class="logo-lg">
+                    <h1>Unlimited films, TV programmes and more.</h1>
+                    <p>Whatch anywhere. Cancel at any time.</p>
+                    <p>Ready to watch?</p>
+                </div>
             </div>
             <div class="login-container col-lg-6 col-md-12 center">
                 <div class="login-form col-lg-8 col-md-8">
@@ -46,6 +36,4 @@
             </div>
         </div>
     </body>
-</html>
-</body>
 </html>
