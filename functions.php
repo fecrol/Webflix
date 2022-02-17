111a6d6c775acb4f8ee6a056b03a4a0f5fede10a
Maciej <maciejfec1996@gmail.com>
2022-02-17 13:07:40 +0000

    added function to retrieve user info from the database. added a load function to redirect user to a specific page. added a login function to execute the login action.

--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@ function validatePassword($email, $password) {
     $r = mysqli_query($link, $q);
     
     if(mysqli_num_rows($r) == 1) {
-        $row = mysqli_fetch_array($r, MYSQLI_ASSOC)
+        $row = mysqli_fetch_array($r, MYSQLI_ASSOC);
 
         if($password == $row["password"]) {
             return true;
@@ -45,5 +45,57 @@ function validatePassword($email, $password) {
     }
 
     mysqli_close($link);
+}
+
+function getUserInfo($email, $password) {
+    /*
+    Gets the user info from the databse to be used within the session.
+    */
+
+    require("connect_db.php");
+
+    $q = "SELECT id, firstName, lastName FROM users WHERE email='$email' AND password='$password'";
+    $r = mysqli_query($link, $q);
+
+    $row = mysqli_fetch_array($r, MYSQLI_ASSOC);
+
+    mysqli_close($link);
+    
+    return $row;
+}
+
+function load($page = "login.php") {
+    /*
+    Loads the specified page to redirect the user.
+    */
+
+    $url = "http://" . $_SERVER["HTTP_HOST"] . dirname($_SERVER["PHP_SELF"]);
+
+    $url = rtrim($url, "/\\");
+    $url .= "/" . $page;
+
+    header("Location: $url");
+    exit();
+}
+
+function login() {
+
+    if($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        $emailVal = validateEmail($email);
+        $passwordVal = validatePassword($email, $password);
+
+        if($emailVal && $passwordVal) {
+            session_start();
+            $userInfo = getUserInfo($email, $password);
+
+            $_SESSION["user_id"] = $userInfo["id"];
+            $_SESSION["firstName"] = $userInfo["firstName"];
+            $_SESSION["lastName"] = $userInfo["lastName"];
+            load("home.php");
+        }
+    }
 }
 ?>
