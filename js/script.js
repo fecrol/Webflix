64f47e20c0ba757782249a792fd092b1759df0e9
Maciej <maciejfec1996@gmail.com>
2022-03-06 12:50:20 +0000

    added function to create option element for the select element. added function to get current year. added function to fill in expiry year select element on registration page

--- js/script.js
+++ js/script.js
@@ -1,27 +1,52 @@
-function fillExpiryMonths() {
-    var selectElement = document.getElementById("expMonth");
+function createOptionElement(value, innerHTML) {
+    var option = document.createElement("option");
+    option.value = value;
+    option.innerHTML = innerHTML;
 
-    for(let i=1; i<=12; i++) {
-        var option = document.createElement("option");
-        option.value = i;
-        option.innerHTML = addZeroIfNumLessThan10(i);
-        selectElement.appendChild(option);
-    }
+    return option;
 }
 
 function addZeroIfNumLessThan10(num) {
-    var newNum;
-    
     if(num < 10) {
-        newNum = "0" + num;
-        return newNum;
+        num = "0" + num;
     }
 
     return num;
 }
 
-function fillExpiryYears() {
-    for(let i=1; i<=10; i++) {
+function fillExpiryMonths() {
+    /*
+    Fills in the expiry months select menu on registration page
+    to allow user to select options 
+    */
+
+    var selectElement = document.getElementById("expMonthOpt");
+
+    for(let i=1; i<=12; i++) {
+        var option = createOptionElement(i, addZeroIfNumLessThan10(i));
         
+        selectElement.appendChild(option);
+    }
+}
+
+function getCurrentYear() {
+    var currentTime = new Date();
+    var currentYear = currentTime.getUTCFullYear();
+
+    return currentYear;
+}
+
+function fillExpiryYears() {
+    /*
+    Fills in the expiry year select menu on registration page
+    to allow user to select options 
+    */
+
+    var year = getCurrentYear();
+    var selectElement = document.getElementById("expYearOpt");
+
+    for(let i=0; i<=4; i++) {
+        var option = createOptionElement(year + i, year + i);
+        selectElement.appendChild(option)
     }
 }
