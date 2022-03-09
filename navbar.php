d528d2e6268060bc33f6722d8fccbdba69839027
Maciej <maciejfec1996@gmail.com>
2022-03-09 16:55:30 +0000

    turned navbar into .php to use session data to display users full name.

---
+++ navbar.php
@@ -0,0 +1,49 @@
+<?php
+session_start(); 
+?>
+
+<nav class="navbar navbar-expand-lg navbar-dark dark-bg">
+    <div class="container-fluid">
+        <a class="navbar-brand nav-logo-link-lg nav-logo-link-md nav-logo-link-sm" href="./home.php"><img
+                src="./img/logo.png" alt="" class="nav-logo"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="./home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./movies.php">Movies</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./tv-shows.php">TV Shows</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./coming_soon.php">Coming Soon</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <?php echo $_SESSION['firstName'] . " " . $_SESSION["lastName"] ?>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="login.php" class="nav-link">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
