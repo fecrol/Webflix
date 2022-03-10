3bfb66d5898778c9db676f354653ab010e9891ac
Maciej <maciejfec1996@gmail.com>
2022-03-10 20:37:36 +0000

    added function which retrievs the api key from the api-key.json file to make api calls

--- js/tmdb-script.js
+++ js/tmdb-script.js
@@ -1,5 +1,13 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY = "?api_key=";
+const API_KEY = retrieveApiKey();
+
+function retrieveApiKey() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "./misc/api-key.json", false);
+    xhr.send();
+    var apiKeyJson = JSON.parse(xhr.responseText);
+    return apiKeyJson["api-key"];
+}
 
 function getContent(type, id) {
     url = BASE_URL + type + "/" + id + API_KEY;
