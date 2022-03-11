c28196d08c065f577a7a37ac0b9efdfe37b7aa43
Maciej <maciejfec1996@gmail.com>
2022-03-11 11:45:54 +0000

    added a show content function to test if api displays anything

--- js/tmdb-script.js
+++ js/tmdb-script.js
@@ -1,5 +1,8 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY = retrieveApiKey();
+const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+const cards = document.getElementById("cards");
 
 function retrieveApiKey() {
     var xhr = new XMLHttpRequest();
@@ -10,9 +13,25 @@ function retrieveApiKey() {
 }
 
 function getContent(type, id) {
-    url = BASE_URL + type + "/" + id + API_KEY;
+    const url = BASE_URL + type + "/" + id + API_KEY;
 
     fetch(url).then(res => res.json()).then(data => {
         console.log(data);
+        showContent(data);
     })
 }
+
+function showContent(data) {
+    cards.innerHTML = "";
+
+    const title = data["title"] 
+    const poster_path = data["poster_path"];
+    const card = document.createElement("div");
+    card.innerHTML = 
+        `
+        <img src="${IMG_BASE_URL + poster_path}" alt="${title}">
+        `
+    cards.appendChild(card);
+}
+
+getContent("movie", 11);
