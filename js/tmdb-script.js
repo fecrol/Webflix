2ce23e7254bf374c30d5aa410cb19b8accf65db7
Maciej <maciejfec1996@gmail.com>
2022-03-15 18:01:38 +0000

    rewrote api call function and added function to display movies in carousel on home page

--- js/tmdb-script.js
+++ js/tmdb-script.js
@@ -2,7 +2,7 @@ const BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY = retrieveApiKey();
 const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
-const cards = document.getElementById("cards");
+const carousel_inner = document.getElementById("carousel-inner");
 
 function retrieveApiKey() {
     var xhr = new XMLHttpRequest();
@@ -12,22 +12,44 @@ function retrieveApiKey() {
     return apiKeyJson["api-key"];
 }
 
-function getContent(type, id) {
-    const url = BASE_URL + type + "/" + id + API_KEY;
+function addCardsToCarousel(type, id, isFirstCard) {
+    const url = BASE_URL + type + "/" + id + API_KEY; 
 
     fetch(url).then(res => res.json()).then(data => {
-        showContent(data);
+        const title = data["title"];
+        const overview = data["overview"];
+        const poster_path = data["poster_path"];
+        const carousel_item = document.createElement("div");
+        carousel_item.classList.add("carousel-item");
+
+        if(isFirstCard) {
+            carousel_item.classList.add("active");
+        }
+
+        carousel_item.innerHTML = 
+            `
+            <div class="card">
+                <div class="row">
+                    <div class="card-image col-lg-3 col-md-4">
+                        <img src="${IMG_BASE_URL + poster_path}" alt="${title}">
+                    </div>
+                    <div class="card-content col-lg-9 col-md-8">
+                        <h1>${title}</h1>
+                        <p>${overview}</p>
+                    </div>
+                </div>
+            </div>
+            `
+        carousel_inner.appendChild(carousel_item);
     })
 }
 
-function showContent(data) {
-    const title = data["title"] 
-    const poster_path = data["poster_path"];
-    const card = document.createElement("div");
-    card.classList.add("card");
-    card.innerHTML = 
-        `
-        <img src="${IMG_BASE_URL + poster_path}" alt="${title}">
-        `
-    cards.appendChild(card);
+function displayCarousel() {
+    var content = [{"type": "movie", "id": 2}, {"type": "movie", "id": 3}, {"type": "movie", "id": 5}, {"type": "movie", "id": 6}, {"type": "movie", "id": 12}, {"type": "movie", "id": 13}];
+
+    addCardsToCarousel(content[0]["type"], content[0]["id"], true);
+
+    for(let i=1; i<content.length; i++) {
+            addCardsToCarousel(content[i]["type"], content[i]["id"]);
+        }
 }
