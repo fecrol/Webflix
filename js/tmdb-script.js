105b4be109c2f303a1e7741f0a1cfda362f2088b
Maciej <maciejfec1996@gmail.com>
2022-03-16 10:58:14 +0000

    added if statement to change title based on whether content is a movie or tv series. added some code to display 6 series for testing.

--- js/tmdb-script.js
+++ js/tmdb-script.js
@@ -2,7 +2,8 @@ const BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY = retrieveApiKey();
 const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
-const carousel_inner = document.getElementById("carousel-inner");
+const movie_carousel = document.getElementById("movie-carousel");
+const tv_carousel = document.getElementById("tv-carousel");
 
 function retrieveApiKey() {
     var xhr = new XMLHttpRequest();
@@ -16,13 +17,22 @@ function addCardToCarousel(type, id, parent_container, is_first_card) {
     const url = BASE_URL + type + "/" + id + API_KEY;
 
     fetch(url).then(res => res.json()).then(data => {
+        console.log(data);
         createCarouselCard(data, parent_container, is_first_card);
     })
 }
 
 function createCarouselCard(data, parent_container, is_frist_card) {
     
-    const title = data["title"];
+    var title = "";
+    
+    if(!data["title"]) {
+        title = data["original_name"];
+    }
+    else {
+        title = data["title"];
+    }
+    
     const overview = data["overview"];
     const poster_path = data["poster_path"];
     const carousel_item = document.createElement("div");
@@ -53,9 +63,17 @@ function createCarouselCard(data, parent_container, is_frist_card) {
 
 var content = [{"type": "movie", "id": 2}, {"type": "movie", "id": 3}, {"type": "movie", "id": 5}, {"type": "movie", "id": 6}, {"type": "movie", "id": 12}, {"type": "movie", "id": 13}];
 
-addCardToCarousel(content[0]["type"], content[0]["id"], carousel_inner, true);
+var content2 = [{"type": "tv", "id": 2}, {"type": "tv", "id": 3}, {"type": "tv", "id": 5}, {"type": "tv", "id": 6}, {"type": "tv", "id": 500}, {"type": "tv", "id": 13}];
+
+addCardToCarousel(content[0]["type"], content[0]["id"], movie_carousel, true);
 
 for(let i=1; i<content.length; i++) {
-    addCardToCarousel(content[i]["type"], content[i]["id"], carousel_inner, false);
+    addCardToCarousel(content[i]["type"], content[i]["id"], movie_carousel, false);
+}
+
+addCardToCarousel(content2[0]["type"], content2[0]["id"], tv_carousel, true);
+
+for(let i=1; i<content2.length; i++) {
+    addCardToCarousel(content2[i]["type"], content2[i]["id"], tv_carousel, false);
 }
 
