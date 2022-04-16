59cd2c07607f5281aaaf6d7d47f23f944556bde2
Maciej Fec <maciejfec1996@gmail.com>
2022-04-16 15:09:31 +0100

    changed element id from movies to content

--- js/api-script.js
+++ js/api-script.js
@@ -2,7 +2,7 @@ const API_KEY = retrieveApiKey();
 const BASE_URL = "https://api.themoviedb.org/3/";
 const IMG_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
-const parentContainer = document.getElementById("movies");
+const parentContainer = document.getElementById("content");
 
 function retrieveApiKey() {
     /*
