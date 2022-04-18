694b6e94bec66d8e5bf5c7768904b7313b80202d
Maciej Fec <maciejfec1996@gmail.com>
2022-04-18 12:58:53 +0100

    added code to display the data stored in the database using the tmdb api

--- api-scripts/get-coming-soon.php
+++ api-scripts/get-coming-soon.php
@@ -1,2 +1,17 @@
 <?php
+$dbData = getComingSoon();
 ?>
+
+<script src="./js/api-script.js">
+</script>
+
+
+<script>
+
+let dbData = <?php echo json_encode($dbData); ?>; 
+
+for(let i=0; i<dbData.length; i++) {
+    fetchContent(dbData[i]["type"], dbData[i]["tmdbId"]);
+}
+
+</script>
