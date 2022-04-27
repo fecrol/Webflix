6f4423e8f3241c680de57acd11d8b98a0b236eef
Maciej Fec <maciejfec1996@gmail.com>
2022-04-27 21:03:24 +0100

    added code to get necessary information and display content

--- api-scripts/get-single-content.php
+++ api-scripts/get-single-content.php
@@ -1,2 +1,18 @@
 <?php
+$contentDetails = getContentDetails();
+$tmdbId = $contentDetails["id"];
+$type = $contentDetails["type"];
+$dbData = getSingleContent($tmdbId, $type);
 ?>
+
+<script src="./js/api-script.js">
+</script>
+
+
+<script>
+
+let dbData = <?php echo json_encode($dbData); ?>; 
+
+fetchSingleContent(dbData["type"], dbData["tmdbId"], dbData["trailer"]);
+
+</script>
