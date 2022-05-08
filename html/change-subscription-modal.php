039d2ceeeb84b683db7c3c1fcd55f008fb9dd018
Maciej Fec <maciejfec1996@gmail.com>
2022-05-08 12:51:13 +0100

    added code to display modal to update subscription

---
+++ html/change-subscription-modal.php
@@ -0,0 +1,21 @@
+<div class="modal fade" id="subscription" tabindex="-1" role="dialog" aria-labelledby="subscription" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <form action="" method="POST">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalCenterTitle">Change Password</h5>
+                </div>
+                <div class="modal-body">
+                    
+                </div>
+                <div class="modal-footer">
+                    <div class="form-group">
+                         <input type="submit" 
+                                       name="btnChangePassword" 
+                                       class="btn btn-dark btn-lg btn-block" value="Save Changes"/>
+                    </div>
+                </div>
+            </form>
+        </div><!--Close body-->
+    </div><!--Close modal-body-->
+</div><!-- Close modal-fade-->
